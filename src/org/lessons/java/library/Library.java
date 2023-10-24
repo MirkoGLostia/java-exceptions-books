417d12790209e5e276f02bf9d1f2376cfa121750
MirkoGLostia <giggi.torre@mail.com>
2023-10-24 14:45:06 +0200

    main class library set array lenght with error exception

--- src/org/lessons/java/library/Library.java
+++ src/org/lessons/java/library/Library.java
@@ -6,6 +6,21 @@ public class Library {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
 
+        // arrays and variables
+        int numberOfBooks;
+        Books[] listOfBooks;
+
+        System.out.println("How many books do you want to add?");
+        numberOfBooks = Integer.parseInt(userInput.nextLine());
+
+        if (numberOfBooks < 0 || numberOfBooks == 0) {
+            System.out.println("you need a positive number");
+            throw new NullPointerException();
+        }
+
+        listOfBooks = new Books[numberOfBooks];
+
+        System.out.println(listOfBooks.length);
 
 
 
