f7b2c7783e19873a71befc3fd7ad122974027074
MirkoGLostia <giggi.torre@mail.com>
2023-10-24 14:27:18 +0200

    main class library

---
+++ src/org/lessons/java/library/Library.java
@@ -0,0 +1,17 @@
+package org.lessons.java.library;
+
+import java.util.Scanner;
+
+public class Library {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+
+
+
+
+
+
+        userInput.close();
+    }
+}
